7ea7466e106e0c72f69985ccb76d7a7f322695f3
Jason <99057964@mydavinci.nl>
2021-09-16 11:29:01 +0200

    Nog bezig.

--- Prototype/prompts/script.js
+++ Prototype/prompts/script.js
@@ -1,6 +1,5 @@
 var hamburger = prompt('Hoeveel hamburgers wilt u bestellen?');
 var aantalHamburgers = hamburger;
 var keuzeFriet = prompt('Welke maat bak friet wilt u? \n klein of medium of groot?');
-    if(keuzeFriet == 'klein'){
-        var KleineFriet = Number(prompt('Hoeveel kleine friet wilt u?'));
-    } 
+
+
